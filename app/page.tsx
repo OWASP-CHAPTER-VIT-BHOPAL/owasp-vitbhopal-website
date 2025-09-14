ee534663504bbf1201003f862ebd84181589da90
Aashish Raj <113645205+aashishrajdev@users.noreply.github.com>
2025-09-14 18:48:59 +0530

    Achievements page done

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,15 @@
 import HeroSection from "@/components/heroSection";
 import EventSection from "@/components/eventSection";
+import Achievement from "@/components/achievement";
+import Footer from "@/components/footer";
 
 export default function Home() {
   return (
     <>
-      <HeroSection/>
-      <EventSection/>
+      <HeroSection />
+      <EventSection />
+      <Achievement />
+      <Footer />
     </>
   );
 }
