ffe36c03120eb823c017b2674de514b4923fe129
Saurabh-2607 <saurabh.nayla@gmail.com>
2025-09-16 03:20:09 +0530

    Remove unused Footer import and UpcomingEvent component; update build script in package.json

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,6 @@
 import HeroSection from "@/components/heroSection";
 import EventSection from "@/components/eventSection";
 import Achievement from "@/components/achievement";
-import Footer from "@/components/footer";
 
 export default function Home() {
   return (
--- components/eventSection.tsx
+++ components/eventSection.tsx
@@ -1,7 +1,7 @@
 "use client";
 import { Container } from "./container";
 import React, { useState } from "react";
-import { upCommingEvents, pastEvents } from "../Content/Events";
+import { pastEvents } from "../Content/Events";
 import Image from "next/image";
 
 interface AccordionProps {
@@ -67,76 +67,76 @@ interface UpcomingEventProps {
   }>;
 }
 
-const UpcomingEvent: React.FC<UpcomingEventProps> = ({ events }) => {
-  return (
-    <div className="w-full rounded-2xl border-2 border-[var(--border)] mt-8 ">
-      <div className="w-full h-full flex flex-col p-6">
-        <div className="flex justify-between items-center">
-          <div className="text-3xl font-medium">Upcoming Event</div>
-          <div>
-            <a
-              href="/events"
-              className="text-md font-medium transition-transform duration-200 group"
-            >
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="24"
-                height="24"
-                viewBox="0 0 24 24"
-                fill="none"
-                stroke="currentColor"
-                strokeWidth="2"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                className="icon icon-tabler icons-tabler-outline icon-tabler-arrow-up-right transition-transform duration-200 group-hover:rotate-0 rotate-45"
-              >
-                <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-                <path d="M17 7l-10 10" />
-                <path d="M8 7l9 0l0 9" />
-              </svg>
-            </a>
-          </div>
-        </div>
-        <div className="w-full h-[2px] bg-[var(--border)] my-4" />
-        {events.map((event, idx) => (
-          <div
-            key={idx}
-            className="flex flex-col md:flex-row gap-6 items-center mb-6"
-          >
-            {event.imgUrl ? (
-              <Image
-                src={event.imgUrl}
-                width={200}
-                height={200}
-                alt={event.title}
-                className="w-full md:w-1/2 h-58 bg-white rounded-2xl aspect-square object-cover"
-              />
-            ) : (
-              <div className="w-full md:w-1/2 h-58 bg-white rounded-2xl aspect-square" />
-            )}
-            <div className="flex flex-col gap-4">
-              <div className="text-2xl font-semibold">{event.title}</div>
-              <div className="text-sm font-normal text-[var(--muted-text)]">
-                {event.description}
-              </div>
-              <div>
-                <div className="text-sm font-medium text-[var(--muted-text)]">
-                  Date: {event.date}
-                </div>
-                <div className="text-sm font-medium text-[var(--muted-text)]">
-                  Time: {event.time}
-                </div>
-                <div className="text-sm font-medium text-[var(--muted-text)]">
-                  Location: {event.venue}
-                </div>
-              </div>
-            </div>
-          </div>
-        ))}
-      </div>
-    </div>
-  );
-};
+// const UpcomingEvent: React.FC<UpcomingEventProps> = ({ events }) => {
+//   return (
+//     <div className="w-full rounded-2xl border-2 border-[var(--border)] mt-8 ">
+//       <div className="w-full h-full flex flex-col p-6">
+//         <div className="flex justify-between items-center">
+//           <div className="text-3xl font-medium">Upcoming Event</div>
+//           <div>
+//             <a
+//               href="/events"
+//               className="text-md font-medium transition-transform duration-200 group"
+//             >
+//               <svg
+//                 xmlns="http://www.w3.org/2000/svg"
+//                 width="24"
+//                 height="24"
+//                 viewBox="0 0 24 24"
+//                 fill="none"
+//                 stroke="currentColor"
+//                 strokeWidth="2"
+//                 strokeLinecap="round"
+//                 strokeLinejoin="round"
+//                 className="icon icon-tabler icons-tabler-outline icon-tabler-arrow-up-right transition-transform duration-200 group-hover:rotate-0 rotate-45"
+//               >
+//                 <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+//                 <path d="M17 7l-10 10" />
+//                 <path d="M8 7l9 0l0 9" />
+//               </svg>
+//             </a>
+//           </div>
+//         </div>
+//         <div className="w-full h-[2px] bg-[var(--border)] my-4" />
+//         {events.map((event, idx) => (
+//           <div
+//             key={idx}
+//             className="flex flex-col md:flex-row gap-6 items-center mb-6"
+//           >
+//             {event.imgUrl ? (
+//               <Image
+//                 src={event.imgUrl}
+//                 width={200}
+//                 height={200}
+//                 alt={event.title}
+//                 className="w-full md:w-1/2 h-58 bg-white rounded-2xl aspect-square object-cover"
+//               />
+//             ) : (
+//               <div className="w-full md:w-1/2 h-58 bg-white rounded-2xl aspect-square" />
+//             )}
+//             <div className="flex flex-col gap-4">
+//               <div className="text-2xl font-semibold">{event.title}</div>
+//               <div className="text-sm font-normal text-[var(--muted-text)]">
+//                 {event.description}
+//               </div>
+//               <div>
+//                 <div className="text-sm font-medium text-[var(--muted-text)]">
+//                   Date: {event.date}
+//                 </div>
+//                 <div className="text-sm font-medium text-[var(--muted-text)]">
+//                   Time: {event.time}
+//                 </div>
+//                 <div className="text-sm font-medium text-[var(--muted-text)]">
+//                   Location: {event.venue}
+//                 </div>
+//               </div>
+//             </div>
+//           </div>
+//         ))}
+//       </div>
+//     </div>
+//   );
+// };
 
 const EventSection = () => {
   const [openIndex, setOpenIndex] = useState<number>(1);
