b5d866d0e1e5ee13232cb5cd8820354bdd4c56d7
Raunak Sachdeva <raunakdachdeva2004@gmail.com>
2025-09-23 18:43:19 +0530

    Implement code changes to events page

--- Content/Events.ts
+++ Content/Events.ts
@@ -9,46 +9,83 @@ const upCommingEvents = [
     time: "10:00 AM",
     venue: "VIT Bhopal University",
   },
-  {
-    title: "Shell N' Zen",
-    date: "2025-09-10",
-    description:
-      "Join us for the annual OWASP AppSec India conference, featuring top speakers and workshops on application security.",
-    link: "https://www.owasp.org/events/appsec-india-2024/",
-    imgUrl: "/testimg1.png",
-    time: "10:00 AM",
-    venue: "VIT Bhopal University",
-  },
 ];
 
 const pastEvents = [
   {
-    title: "OWASP AppSec India 2024",
-    date: "2024-09-10",
+    title: "CyberConclave 2024",
+    date: "2024-02-29 & 2024-03-01",
     description:
-      "Join us for the annual OWASP AppSec India conference, featuring top speakers and workshops on application security.",
-    link: "https://www.owasp.org/events/appsec-india-2024/",
-    imgUrl: "/testimg1.png",
+      "CyberConclave 2024 was VIT Bhopal's premier cybersecurity conference, featuring a full day of insightful talks and hands-on workshops from industry experts.",
+    link: "https://www.instagram.com/cyberconclave.vitb?utm_source=ig_web_button_share_sheet&igsh=ZDNlZDc0MzIxNw==",
+    imgUrl: "/events/cyberconclave.JPG",
     time: "10:00 AM - 4:00 PM",
   },
   {
-    title: "OWASP AppSec India 2025",
-    date: "2024-09-10",
+    title: "Shell n’ Zen 2024",
+    date: "2025-02-20",
     description:
-      "Join us for the annual OWASP AppSec India conference, featuring top speakers and workshops on application security.",
-    link: "https://www.owasp.org/events/appsec-india-2024/",
-    imgUrl: "/testimg1.png",
-    time: "10:00 AM",
+      "Our Shell n' Zen event engaged aspiring security enthusiasts with a packed workshop and a massive online CTF that challenged hundreds of participants to dominate the leaderboard.",
+    link: "https://www.instagram.com/p/DGd3jopT3u_/",
+    imgUrl: "/events/shell.JPG",
+    time: "11:00 AM - 1:30 PM",
   },
   {
-    title: "OWASP AppSec India 2026",
-    date: "2024-09-10",
+    title: "BugTrek",
+    date: "2024-11-26",
     description:
-      "Join us for the annual OWASP AppSec India conference, featuring top speakers and workshops on application security.",
-    link: "https://www.owasp.org/events/appsec-india-2024/",
-    imgUrl: "/testimg1.png",
-    time: "10:00 AM",
+      "BUGTREK featured an exclusive session with renowned Bug Bounty Hunter, Ms. Aksha Chudasama, who shared her insights on security research before a thrilling CTF competition.",
+    link: "https://www.instagram.com/p/DCi8Iw4vdNQ/",
+    imgUrl: "/events/bugtrek.JPG",
+    time: "11:00 AM - 4:00 PM",
   },
+  {
+    title: "101 with ML and revisiting Cyber Security",
+    date: "2024-04-28",
+    description:
+      "101 with Machine Learning & Revisiting Cyber Security” session presented by OWASP Bhopal Chapter & VIT-OWASP in Collaboration with TensorFlow Users Group Bhopal",
+    link: "https://www.instagram.com/p/C6JGYRKvtKw/?img_index=1",
+    imgUrl: "/events/101.JPG",
+    time: "11:00 AM - 4:30 PM",
+  },
+  {
+    title: "Cyber Scavenger Hunt CTF",
+    date: "2024-02-23",
+    description:
+      "OWASP Club onsite CTF event on the occassion of Advitya 2024.",
+    link: "https://www.instagram.com/p/C3Ry3fMvg8S/",
+    imgUrl: "/events/scavenger.JPG",
+    time: "3:00 PM - 5:30 PM",
+  },
+  {
+    title: "Decode-ए-Cyber",
+    date: "2023-11-04 - 2023-11-05",
+    description:
+      "A special two-day event hosted by OWASP VITB Club in collaboration with Null VIT Bhopal Student Chapter on November 4th and 5th.",
+    link: "https://www.instagram.com/p/C0_Mz4loJme/",
+    imgUrl: "/events/decode.JPG",
+    time: "11:00 AM - 1:30 PM",
+  },
+  {
+    title: "Cy-VITya",
+    date: "2023-02-10",
+    description:
+      "Cyber Security Event held as part of AdVITya, the annual fest of VIT Bhopal",
+    link: "https://www.youtube.com/watch?v=VCn4hJBoNtU",
+    imgUrl: "/events/cyvitya.JPG",
+    time: "10:00 AM - 1:00 PM",
+  },
+  {
+    title: "Inaugural Ceremony",
+    date: "2022-10-01",
+    description:
+      "Official Inauguration Program of OWASP VIT Bhopal University - Student Chapter",
+    link: "https://www.youtube.com/watch?v=u4OXOqkjIQg",
+    imgUrl: "/events/inaugral.JPG",
+    time: "3:00 PM - 4:00 PM",
+  },
+
 ];
 
-export { upCommingEvents, pastEvents };
+export { pastEvents, upCommingEvents };
+
